f3157e75bc9c2e00a482359e31b2cb4c1575ff23
Elliot Murphy <statik@users.noreply.github.com>
2019-12-05 22:54:57 -0500

    get bazel output into workspace dir
    bazel builds into $HOME/.cache/bazel, and github actions
    don't preserve that tree between job steps
    
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- goreleaser-post-hook.sh
+++ goreleaser-post-hook.sh
@@ -3,6 +3,6 @@ set -eo pipefail
 
 echo "moving bazel outputs to goreleaser dist directory for packaging..."
 
-cp bazel-bin/darwin_amd64_pure_stripped/deleterious dist/deleterious_darwin_amd64/
-cp bazel-bin/linux_amd64_pure_stripped/deleterious dist/deleterious_linux_amd64/
+cp bdist/deleterious-darwin dist/deleterious_darwin_amd64/deleterious
+cp bdist/deleterious-linux dist/deleterious_linux_amd64/deleterious
 
---
+++ install.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+
+set -eo pipefail
+tar xf "$1" -C "${BUILD_WORKSPACE_DIRECTORY}"/bdist

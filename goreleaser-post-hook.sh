4538aa8bb84195084f6f6d5a06dcba2821da97b4
Elliot Murphy <statik@users.noreply.github.com>
2020-05-16 20:22:37 -0400

    Adjust goreleaser post hook to create directories
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- goreleaser-post-hook.sh
+++ goreleaser-post-hook.sh
@@ -2,6 +2,9 @@
 set -eo pipefail
 
 echo "moving bazel outputs to goreleaser dist directory for packaging..."
+mkdir -p dist/deleterious_darwin_amd64
+mkdir -p dist/deleterious_linux_amd64
+mkdir -p dist/deleterious_windows_amd64
 
 cp bdist/deleterious-darwin dist/deleterious_darwin_amd64/deleterious
 cp bdist/deleterious-linux dist/deleterious_linux_amd64/deleterious

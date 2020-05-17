b8c10548f558950ec4be5c85395916e4432727fe
Elliot Murphy <statik@users.noreply.github.com>
2020-05-16 20:31:08 -0400

    GitHub actions passwordless sudo
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- goreleaser-post-hook.sh
+++ goreleaser-post-hook.sh
@@ -6,7 +6,7 @@ mkdir -p dist/deleterious_darwin_amd64
 mkdir -p dist/deleterious_linux_amd64
 mkdir -p dist/deleterious_windows_amd64
 
-cp bdist/deleterious-darwin dist/deleterious_darwin_amd64/deleterious
-cp bdist/deleterious-linux dist/deleterious_linux_amd64/deleterious
-cp bdist/deleterious-windows.exe dist/deleterious_windows_amd64/deleterious.exe
+sudo cp bdist/deleterious-darwin dist/deleterious_darwin_amd64/deleterious
+sudo cp bdist/deleterious-linux dist/deleterious_linux_amd64/deleterious
+sudo cp bdist/deleterious-windows.exe dist/deleterious_windows_amd64/deleterious.exe
 

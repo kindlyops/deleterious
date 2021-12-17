1849c6edcb2cd959f0180d369edd8ea4bd8b9227
Elliot Murphy <statik@users.noreply.github.com>
2021-12-17 14:21:44 -0500

    enable CI run on main as well
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- main_test.go
+++ main_test.go
@@ -53,3 +53,31 @@ func TestCLIVersion(t *testing.T) {
 		t.Error("Expected", "dev.bazel version", "got", output)
 	}
 }
+
+func TestCLIHelp(t *testing.T) {
+	t.Parallel()
+
+	path, err := bazel.Runfile(*cli)
+	if err != nil {
+		t.Fatalf("Could not find runfile %s: %q", *cli, err)
+	}
+
+	if _, err = os.Stat(path); os.IsNotExist(err) {
+		t.Fatalf("Missing binary %v", path)
+	}
+	file, err := filepath.EvalSymlinks(path)
+	if err != nil {
+		t.Fatalf("Invalid filename %v", path)
+	}
+	cmd := exec.Command(file, "--help")
+	cmd.Stderr = os.Stderr
+	res, err := cmd.Output()
+	if err != nil {
+		t.Fatalf("failed running '%v': %v", path, err)
+	}
+	output := strings.TrimSpace(string(res))
+
+	if strings.Contains(output, "UsageA:") == false {
+		t.Error("Expected: ", "'Usage:'", "Got:\n", output)
+	}
+}

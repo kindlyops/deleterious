daee569dcd94a0cffc7e6a2879652bd9080e1070
Elliot Murphy <statik@users.noreply.github.com>
2021-12-21 22:53:01 -0500

    goreleaser fixes
    - fix linux arm release
    - add discord notifications of releases
    - fix race condition in post hook
    - experiment with different sbom settings
    
    Signed-off-by: Elliot Murphy <statik@users.noreply.github.com>

--- goreleaser-post-hook.sh
+++ goreleaser-post-hook.sh
@@ -5,9 +5,11 @@ echo "moving bazel outputs to goreleaser dist directory for packaging..."
 mkdir -p dist/deleterious_darwin_amd64
 mkdir -p dist/deleterious_darwin_arm64
 mkdir -p dist/deleterious_linux_amd64
+mkdir -p dist/deleterious_linux_arm64
 mkdir -p dist/deleterious_windows_amd64
 
-sudo cp bdist/deleterious-darwin dist/deleterious_darwin_amd64/deleterious
-sudo cp bdist/deleterious-darwin-m1 dist/deleterious_darwin_arm64/deleterious
-sudo cp bdist/deleterious-linux dist/deleterious_linux_amd64/deleterious
-sudo cp bdist/deleterious-windows.exe dist/deleterious_windows_amd64/deleterious.exe
+sudo cp -f bdist/deleterious-darwin dist/deleterious_darwin_amd64/deleterious
+sudo cp -f bdist/deleterious-darwin-m1 dist/deleterious_darwin_arm64/deleterious
+sudo cp -f bdist/deleterious-linux dist/deleterious_linux_amd64/deleterious
+sudo cp -f bdist/deleterious-linux-arm dist/deleterious_linux_arm64/deleterious
+sudo cp -f bdist/deleterious-windows.exe dist/deleterious_windows_amd64/deleterious.exe
